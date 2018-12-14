ff75b10dced655fbfa752bbe45a428b4de6686d2
Corentin Prune <corentin@kicklox.com>
2018-12-14 22:00:43 +0100

    add ids to messages, sendMessage api method

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,24 @@
 import channels from "./channels.json";
 import messages from "./messages.json";
+import uuid from "uuid/v1";
 
 // use fake timeouts to simulate API call
 
 export const getChannels = () =>
-  new Promise(resolve => setTimeout(resolve, 2000, channels.data));
+  new Promise(resolve => {
+    setTimeout(resolve, 2000, channels.data);
+  });
 
 export const getChannelMessages = channelId =>
-  new Promise(resolve =>
-    setTimeout(resolve, 3000, messages.filter(m => m.channel === channelId))
-  );
+  new Promise(resolve => {
+    setTimeout(
+      resolve,
+      3000,
+      messages.data.filter(m => m.channel === channelId)
+    );
+  });
+
+export const sendMessageToChannel = ({ channel, content }) =>
+  new Promise(resolve => {
+    setTimeout(resolve, 600, { id: uuid(), ts: Date.now(), content, channel });
+  });
