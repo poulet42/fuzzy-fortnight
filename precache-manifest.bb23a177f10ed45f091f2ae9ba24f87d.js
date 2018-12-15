5dbe16946e521c7e41b666a6d3211f76f1837be1
Corentin Prune <corentin@kicklox.com>
2018-12-15 01:22:55 +0100

    Updates

---
+++ precache-manifest.bb23a177f10ed45f091f2ae9ba24f87d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f42005ef0b9864b698b6",
+    "url": "/fuzzy-fortnight/static/css/main.74a122ef.chunk.css"
+  },
+  {
+    "revision": "f42005ef0b9864b698b6",
+    "url": "/fuzzy-fortnight/static/js/main.f42005ef.chunk.js"
+  },
+  {
+    "revision": "b52f3d7ed6dbaee5768d",
+    "url": "/fuzzy-fortnight/static/js/1.b52f3d7e.chunk.js"
+  },
+  {
+    "revision": "9f262e2a0c3c069cb8eb",
+    "url": "/fuzzy-fortnight/static/js/runtime~main.9f262e2a.js"
+  },
+  {
+    "revision": "65d317c9a3f7c637b06fd8b736b0d727",
+    "url": "/fuzzy-fortnight/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/test-chat/precache-manifest.cf066c5f29b5e1477800ee04f851167f.js"
+  "/fuzzy-fortnight/precache-manifest.bb23a177f10ed45f091f2ae9ba24f87d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/test-chat/index.html", {
+workbox.routing.registerNavigationRoute("/fuzzy-fortnight/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
