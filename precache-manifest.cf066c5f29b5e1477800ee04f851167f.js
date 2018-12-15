f7adca22e6e72ae1306b441d6d82420cd8a822b4
Corentin Prune <corentin@kicklox.com>
2018-12-15 01:19:27 +0100

    Updates

---
+++ precache-manifest.cf066c5f29b5e1477800ee04f851167f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32c1cf371f8afad24e87",
+    "url": "/test-chat/static/css/main.74a122ef.chunk.css"
+  },
+  {
+    "revision": "32c1cf371f8afad24e87",
+    "url": "/test-chat/static/js/main.32c1cf37.chunk.js"
+  },
+  {
+    "revision": "b52f3d7ed6dbaee5768d",
+    "url": "/test-chat/static/js/1.b52f3d7e.chunk.js"
+  },
+  {
+    "revision": "89118ce2d3d29837fadf",
+    "url": "/test-chat/static/js/runtime~main.89118ce2.js"
+  },
+  {
+    "revision": "527b08722369959a09a00549a767e6b2",
+    "url": "/test-chat/index.html"
+  }
+];
